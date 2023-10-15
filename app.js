975cd8715ceaba5ba6ceee66bd209b5f81aca134
Zee <Zeeshanmb10@gmail.com>
2023-10-15 15:32:02 -0400

    Finished Moto page, added a slider

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+
+
+// Get the slider and slides
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+// Function to show a specific slide
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+// Function to go to the next slide
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto-play the slider
+setInterval(nextSlide, 3000); // Change slides every 3 seconds (adjust as needed)
+
+// Show the initial slide
+showSlide(currentSlide);
