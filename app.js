df214d02b012f6b77916d733bb0efbb74868b1dd
Zee <Zeeshanmb10@gmail.com>
2023-10-16 00:41:07 -0400

    Finished Website

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Navbar in mobile menu 
+
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
@@ -6,7 +8,7 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active');
 })
 
-
+// Chaning slide in Our Moto
 
 // Get the slider and slides
 const slider = document.querySelector('.slider');
@@ -36,3 +38,32 @@ setInterval(nextSlide, 3000); // Change slides every 3 seconds (adjust as needed
 
 // Show the initial slide
 showSlide(currentSlide);
+
+
+
+// About us
+// Add this JavaScript code to your app.js file
+var extraContent = document.getElementById("extraContent");
+extraContent.classList.add("hidden-content");
+
+document.getElementById("readMoreBtn").addEventListener("click", function () {
+    if (extraContent.classList.contains("hidden-content")) {
+        extraContent.classList.remove("hidden-content");
+        this.innerText = "Read Less";
+    } else {
+        extraContent.classList.add("hidden-content");
+        this.innerText = "Read More";
+    }
+});
+
+
+function updateBackgroundHeight() {
+  const contentHeight = document.querySelector('.about-container').offsetHeight;
+  document.querySelector('.background-container').style.height = contentHeight + 'px';
+}
+
+// Call the function on page load and when the window is resized
+window.addEventListener('load', updateBackgroundHeight);
+window.addEventListener('resize', updateBackgroundHeight);
+
+
